5a24eadc6f2e0db03ced1731bbcb945a262b5d11
atila593 <108370757+atila593@users.noreply.github.com>
2025-12-02 22:18:46 +0100

    Update run.sh

--- addon/run.sh
+++ addon/run.sh
@@ -1,4 +1,4 @@
-!/usr/bin/with-contenv bashio
+#!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 VERSION="1.1.1"
 ################################################################################
